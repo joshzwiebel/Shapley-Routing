c4ec976427c0343ea3a5094eb8ecf20652996696
Josh Zwiebel <joshzwiebel@gmail.com>
2023-04-09 16:57:39 -0400

    format with black and flake8

--- app/app.py
+++ app/app.py
@@ -6,9 +6,8 @@ from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.distance import distance
 from shapleyrouting.rideshare import RideShare
-from time import sleep
-from random import randint
 import requests
+
 
 @st.cache_resource
 def get_Nominatim():
@@ -16,20 +15,24 @@ def get_Nominatim():
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=5)
     return geocode
 
+
 geocode = get_Nominatim()
+
+
 @st.cache_data
 def geocode_location(location):
     return geocode(location)
 
+
 @st.cache_data
 def requests_geocode(location):
-    # url = f'https://nominatim.openstreetmap.org/search?q={location}&format=json'
     url = f"https://geocode.maps.co/search?q={location}"
     response = requests.get(url)
     if response.status_code == 200 and response.json():
         return response.json()[0]
     else:
         return None
+
 
 num_locations = st.sidebar.slider('Number of locations', 2, 10, 3)
 locations = []
@@ -43,7 +46,9 @@ for i in range(num_locations):
     )
     location_geocoded = requests_geocode(location_option)
     locations.append(location_geocoded)
-cost = st.sidebar.number_input("Total cost of ride", 0.0, 100000.0, 100.0, 0.01)
+cost = st.sidebar.number_input(
+    "Total cost of ride", 0.0, 100000.0, 100.0, 0.01
+)
 run_button = st.sidebar.button('Run')
 if run_button:
     try:
@@ -74,10 +79,15 @@ if run_button:
     ride = RideShare(num_players=num_locations - 1, distances=distance_matrix)
 
     shap_vals = ride.fit()
-    #normalize shap vals to sum to cost
+    # normalize shap vals to sum to cost
     shap_vals = np.array(shap_vals) * cost / np.sum(shap_vals)
 
-    st.write(f"For a trip of {cost} dollars starting at {locations[0]['display_name']}, the optimal split is as follows:")
+    st.write(
+        f"For a trip of {cost} dollars starting at \
+            {locations[0]['display_name']}, the optimal split is as follows:"
+    )
     for i in range(num_locations - 1):
-        st.write(f"Player {i + 1} pays {round(shap_vals[i],2)} dollars to travel to {locations[i + 1]['display_name']}")
-    
+        st.write(
+            f"Player {i + 1} pays {round(shap_vals[i],2)} dollars\
+                 to travel to {locations[i + 1]['display_name']}"
+        )
